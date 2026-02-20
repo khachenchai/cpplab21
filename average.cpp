12b9e66a9d79375169e09cce1602a832a28eecff
khachenchai <jaiklaanda@gmail.com>
2026-02-20 14:55:11 +0700

    Finished Lab21-1

--- average.cpp
+++ average.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+int main(int argc, char *argv[]) {
+    if (argc - 1 <= 0) {
+        cout << "Please input numbers to find average.";
+        return 1;
+    }
+
+    double sum = 0;
+    for (int i = 1; i < argc; i++) {
+        sum += stod(argv[i]);
+    }
+
+    cout << "---------------------------------\n";
+    cout << "Average of " << argc-1 << " numbers = " << (float)(sum / (argc-1)) << endl;
+    cout << "---------------------------------\n";
+    return 0;
+}
